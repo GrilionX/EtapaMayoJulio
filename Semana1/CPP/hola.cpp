0ce9736dd1ab9795530ab5009342e791009e2277
GrilionX <segundodiana37@gmail.com>
2026-05-07 22:48:50 -0600

    Add C++ hello world exampleInclude a basic C++ hello world program withsimple explanations of the main code functionsto support beginner understanding.

---
+++ Semana1/CPP/hola.cpp
@@ -0,0 +1,23 @@
+#include <iostream> //biblioteca estandar de c++
+
+using namespace std;
+
+/*
+using namespace son para importar todos los identificadores repetitivos usados en c++
+tales como lo es std y asi ahorra varias lineas de codigo, y en su lugar el hola mundo quedaria mas complejo
+
+std::cout << "Hola mundo" << endl
+
+habra ocasiones en las cuales no se podria usar esto mismo, pero dependera de la situacion, mas adelante aprenderemos de ellas
+*/
+
+int main(){
+    cout << "Hola mundo" << endl;
+}
+
+
+/*
+/codigo simple en c++ para imprimir un hola munod simple, cout sirve para imrpimir, 
+endl para terminar la linea, experimentarmos con ello mas adelante, a diferencia de c, aqui se usan los '<<'
+para encerrar los strings
+*/
